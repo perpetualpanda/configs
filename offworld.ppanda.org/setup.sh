494a28cdd78961ddffeab10a6c3cc109f5d87e51
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2024-06-16 11:17:31 -0700

    feat: add configs for offworld

---
+++ offworld.ppanda.org/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Author: Yuzhou "Joe" Mo (@yuzhoumo)
+# License: MIT
+
+# upgrade packages and install docker
+apk update && apk upgrade
+apk add docker docker-compose linux-virt linux-virt-dev
+
+# create docker service
+rc-update add docker boot
+service docker start
+
+# install tailscale and authenticate
+curl -fsSL https://tailscale.com/install.sh | sh
+tailscale up --accept-routes --auth-key "${TS_AUTHKEY}"
+
+# bring up docker containers
+docker pull caddy
+docker compose \
+  -f caddy/docker-compose.yml \
+  -f uptime-kuma/docker-compose.yml \
+  up -d
