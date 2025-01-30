e922b673f8e24e53f0e6f1d965fea9b126771ea4
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2025-01-29 23:56:46 -0800

    refactor: consolidate terraform configs

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,23 @@
 terraform {
+  required_version = ">= 1.10"
+
   required_providers {
+    ansible = {
+      version = "1.3.0"
+      source  = "ansible/ansible"
+    }
+
     vultr = {
-      source  = "vultr/vultr"
       version = "2.23.1"
+      source  = "vultr/vultr"
+    }
+  }
+
+  backend "remote" {
+    organization = "perpetualpanda"
+
+    workspaces {
+      name = "infra"
     }
   }
 }
